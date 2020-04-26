f2d374440723d2b3f28612b6a1286eefd48260c3
Harro van der Kroft <wisepotato@users.noreply.github.com>
2020-04-26 18:12:24 +0000

    generalizations

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from statistics import mean
 from datetime import datetime
 from typing import List
 
+import contextlib
 
 
 
@@ -33,14 +34,29 @@ from typing import List
 #     current_wattages = []
 #     client.write_points(json_body)
 
+
+class DmsrFourTelegram:
+    current_usage_electricity: float
+    total_usage_electricity_low: float
+    total_usage_electricity_high: float
+
+class DsmrInfo:
+
+    def __init__(self, variable: str, search_for: str, occurence_number: str, unit: str):
+        self.variable_name = variable
+        self.search_for = search_for
+        self.occurence_number = occurence_number
+        self.unit = unit
+    variable_name: str
+    search_for: str
+    occurence_number: str
+    unit: str
+
 class DsmrFour():
     _serial : serial.Serial
-    _lines: List[str] = []
+    lines: List[str] = []
 
-
-    
-
-
+    _registrations: List[DsmrInfo] = []
     def __init__(self, ser: serial.Serial):
         ser.baudrate = 115200
         ser.bytesize =serial.EIGHTBITS
@@ -49,56 +65,62 @@ class DsmrFour():
         ser.xonxoff = 0
         ser.rtscts = 0
         ser.timeout = 12
-                
         ser.port = "/dev/ttyUSB0"
         self._serial = ser
     
+    def register(self,data_unit: DsmrInfo)  -> None:
+        self._registrations.append(data_unit)
+
+    def interpret(self) -> None:
+        for finder in self._registrations:
+            for text in self.lines:
+                if finder.search_for in text:
+                    groups = re.findall(r'\((.*?)\)',text)
+                    text_thing = str(groups[finder.occurence_number])
+                    if text_thing.endswith(f"*{finder.unit}"):
+                        text_thing = text_thing.replace(f"*{finder.unit}", "")
+                    actual_float = float(text_thing)
+                    print(f"{finder.variable_name} - {actual_float}")
+
     def print_lines(self) -> None:
-        for line in self._lines:
+        for line in self.lines:
             print(line)
-    def append_line(self, line: str) -> None:
-        self._lines.append(line)
-
-    def get_serial(self) -> serial.Serial:
-        return self._serial
-
-class SerialContainer(object):
-    _lines: List[str] = []
-    _serial: serial.Serial
-
-    def __init__(self, dsmr: DsmrFour):
-        self._serial = dsmr.get_serial()
-        self._dsmr = dsmr
-
-    def __enter__(self) -> 'SerialContainer':
-        self._serial.open()
-        
-        while  True:
-            telegram_line = ser.readline()
-            telegram_line = telegram_line.decode('ascii').strip()
-            if self.end_of_telegram(telegram_line):
-                break
-            else:
-                self._dsmr.append_line(telegram_line)
-
-        return self
 
     def end_of_telegram(self, line: str) -> bool:
         return re.match('(?=!)', line)
 
-    def __exit__(self, type, value, traceback):
+
+    @contextlib.contextmanager
+    def start(self) -> 'DsmrFour':
+        self._serial.open()
+
+        while  True:
+            telegram_line = self._serial.readline()
+            telegram_line = telegram_line.decode('ascii').strip()
+            if self.end_of_telegram(telegram_line):
+                break
+            else:
+                self.lines.append(telegram_line)
+
         self._serial.close()
+        yield self
 
 
+dsmr = DsmrFour(serial.Serial())
+data_units = [
+    DsmrInfo(variable="total_gas", search_for="0-1:24.2.1", occurence_number=1, unit="m3"),
 
-ser = serial.Serial()
-dsmr = DsmrFour(ser)
+]
+
+for du in data_units:
+    dsmr.register(du)
 
 while True:
-
-    with SerialContainer(dsmr)  as sc:
-        sc._dsmr.print_lines()
-
+    with dsmr.start() as dsmr:
+        dsmr.lines
+        dsmr.print_lines()    
+        dsmr.interpret()
+    
 
 
     
