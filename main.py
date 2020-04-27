e768c30c296cc4757a0bfb12fc85134808134db3
Harro van der Kroft <wisepotato@users.noreply.github.com>
2020-04-27 16:51:46 +0000

    refined the configuration, actually made an influxdbimporter, and made dsmr more extensible

--- main.py
+++ main.py
@@ -14,18 +14,15 @@ load_dotenv()
 import os
 database_type = os.getenv("db.type")
 database_host = os.getenv("db.hostname")
-database_port = os.getenv("db.port")
+database_port = int(os.getenv("db.port"))
 database_username = os.getenv("db.username")
 database_password = os.getenv("db.password")
 database_database = os.getenv("db.database")
 
-allowed_database_types = ['influx']
+allowed_database_types = ['influxdb']
 
 if database_type not in allowed_database_types:
     raise Exception(f"The database_type {database_type} is not allowed. Allowed types: {allowed_database_types}")
-
-
-
 
 from influxdb import InfluxDBClient
 
@@ -35,12 +32,15 @@ client = InfluxDBClient(
     username=database_username,
     password=database_password,
     database=database_database
-    )
+)
 
+class InfluxImporter:
+    client: InfluxDBClient
 
-# client = InfluxDBClient()
+    def __init__(self, client: InfluxDBClient):
+        self.client = client
 
-
+    
 # current_wattages = []
 
 # def add_wattage(watt_current: float, watt_total_low: float, watt_total_high: float) -> None:
@@ -97,10 +97,10 @@ class DsmrFour():
     def register(self,data_unit: DsmrInfo)  -> None:
         self._registrations.append(data_unit)
 
-    def interpret(self) -> List:
+    def interpret(self, lines: List[str]) -> List:
         data = []
         for finder in self._registrations:
-            for text in self.lines:
+            for text in lines:
                 if finder.search_for in text:
                     groups = re.findall(rf'([0-9]+[\.]+[0-9]*\*{finder.unit})',text)
 
@@ -113,6 +113,9 @@ class DsmrFour():
                     })
         return data
 
+    def get_lines(self) -> List[str]:
+        return self.lines
+
     def print_lines(self) -> None:
         for line in self.lines:
             print(line)
@@ -121,8 +124,7 @@ class DsmrFour():
         return self.ending_character in line
 
 
-    @contextlib.contextmanager
-    def start(self) -> 'DsmrFour':
+    def wait_for_telegram_and_load(self) -> None:
         self._serial.open()
         self.lines = []
         while  True:
@@ -134,37 +136,21 @@ class DsmrFour():
                 self.lines.append(telegram_line)
 
         self._serial.close()
-        yield self
 
 
 dsmr = DsmrFour(serial.Serial())
 data_units = [
-    DsmrInfo(variable="total_gas", search_for="0-1:24.2.1", unit="m3"),
-    DsmrInfo(variable="current_electricity", search_for="1-0:1.7.0",  unit="kW"),
-    DsmrInfo(variable="total_electricity_high", search_for="1-0:1.8.1", unit="kWh"),
-    DsmrInfo(variable="total_electricity_low", search_for="1-0:1.8.2",unit="kWh"),
+    DsmrInfo(variable_name="total_gas", search_for="0-1:24.2.1", unit="m3"),
+    DsmrInfo(variable_name="current_electricity", search_for="1-0:1.7.0",  unit="kW"),
+    DsmrInfo(variable_name="total_electricity_high", search_for="1-0:1.8.1", unit="kWh"),
+    DsmrInfo(variable_name="total_electricity_low", search_for="1-0:1.8.2", unit="kWh"),
 ]
 
 for du in data_units:
     dsmr.register(du)
 
 while True:
-    with dsmr.start() as dsmr:
-        data = dsmr.interpret()
-    
-
-
-    
-    # checksum_found = False
-    
-    # current_wattage = 0.0
-    # while not checksum_found:
-        
-    #     telegram_line = ser.readline() # Lees een seriele lijn in.
-    #     telegram_line = telegram_line.decode('ascii').strip() # Strip spaties en blanke regels
-    #     if re.match(str('(?=1-0:1.7.0)'), str(telegram_line)): #1-0:1.7.0 = Actueel verbruik in kW
-    #     # 1-0:1.7.0(0000.54*kW)
-    #     kw = telegram_line[10:-4] # Knip het kW gedeelte eruit (0000.54)
-    #     watt = float(kw) * 1000 # vermengvuldig met 1000 voor conversie naar Watt (540.0)
-    #     watt = int(watt) # rond float af naar heel getal (540)\
-    #     current_wattage = watt
+    dsmr.wait_for_telegram_and_load()
+    lines = dsmr.get_lines()
+    interpreted = dsmr.interpret(lines)
+    print(interpreted)
